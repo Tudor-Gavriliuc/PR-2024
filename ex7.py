dcec200bf74e450cfdbe97cd40a6dd1b0406be8f
Tudor-Gavriliuc <tudorgavriliuc.2018@gmail.com>
2024-10-17 19:19:17 +0300

    commit

--- ex7.py
+++ ex7.py
@@ -1,8 +1,3 @@
-#  Instead of using the library from grade 2, use
-#  a TCP socket for connection, send an HTTP
-# formatted request, select the HTTP response
-#  body, and pass it further to the scraping logic.
-
 import socket
 import ssl
 from bs4 import BeautifulSoup
@@ -47,7 +42,6 @@ if header_end_idx != -1:
 soup = BeautifulSoup(html_body, 'html.parser')
 
 def scrape_product_details(product_link):
-    # Use socket connection to fetch product details instead of requests
     with socket.create_connection((host, port)) as sock:
         with context.wrap_socket(sock, server_hostname=host) as ssl_sock:
             request = f"GET {product_link} HTTP/1.1\r\nHost: {host}\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/129.0.0.0 Safari/537.36\r\nConnection: close\r\n\r\n"
@@ -60,10 +54,7 @@ def scrape_product_details(product_link):
                     break
                 product_response_data += chunk
 
-    # Decode the response
     product_response_text = product_response_data.decode()
-
-    # Find the start of the HTTP body (after headers)
     header_end_idx = product_response_text.find("\r\n\r\n")
     if header_end_idx != -1:
         product_html_body = product_response_text[header_end_idx + 4:]  # The actual HTML content
@@ -71,7 +62,6 @@ def scrape_product_details(product_link):
         return "Descriere indisponibilă"
 
     product_soup = BeautifulSoup(product_html_body, 'html.parser')
-
     description_tag = product_soup.find('div', class_='adPage__content__description grid_18', itemprop='description')
 
     if description_tag:
@@ -93,7 +83,7 @@ products = soup.find_all('li', class_='ads-list-photo-item')
 
 product_data = []
 for index, product in enumerate(products):
-    if index >= 10:
+    if index >= 20:
         break
 
     name_tag = product.find('div', class_='ads-list-photo-item-title')
@@ -151,4 +141,57 @@ results = {
     'total_price_eur': total_price_eur,
 }
 
-print(results)
+# JSON Serialization
+def serialize_to_json(data):
+    json_string = '{'
+    json_string += f'"timestamp": "{data["timestamp"]}",'
+    json_string += '"filtered_products": ['
+    
+    for product in data['filtered_products']:
+        json_string += '{'
+        json_string += f'"name": "{product["name"]}",'
+        json_string += f'"link": "{product["link"]}",'
+        json_string += f'"price_mdl": {product["price_mdl"]},'
+        json_string += f'"price_eur": {product["price_eur"]},'
+        json_string += f'"description": "{product["description"].replace("\"", "\\\"")}"'  # Escape quotes
+        json_string += '},'
+    
+    if data['filtered_products']:
+        json_string = json_string[:-1]  # Remove last comma
+    
+    json_string += '],'
+    json_string += f'"total_price_eur": {total_price_eur}'
+    json_string += '}'
+    
+    return json_string
+
+# XML Serialization
+def serialize_to_xml(data):
+    xml_string = '<results>'
+    xml_string += f'<timestamp>{data["timestamp"]}</timestamp>'
+    xml_string += '<filtered_products>'
+    
+    for product in data['filtered_products']:
+        xml_string += '<product>'
+        xml_string += f'<name>{product["name"]}</name>'
+        xml_string += f'<link>{product["link"]}</link>'
+        xml_string += f'<price_mdl>{product["price_mdl"]}</price_mdl>'
+        xml_string += f'<price_eur>{product["price_eur"]}</price_eur>'
+        xml_string += f'<description>{product["description"].replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")}</description>'  # Escape XML special characters
+        xml_string += '</product>'
+    
+    xml_string += '</filtered_products>'
+    xml_string += f'<total_price_eur>{total_price_eur}</total_price_eur>'
+    xml_string += '</results>'
+    
+    return xml_string
+
+# Serialize results
+json_output = serialize_to_json(results)
+xml_output = serialize_to_xml(results)
+
+# Print/log outputs
+print("JSON Output:")
+print(json_output)
+print("\nXML Output:")
+print(xml_output)
